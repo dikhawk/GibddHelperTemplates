572dfe98fa09e39147a590c4eab425db8f195e33
dikhawk <dik_hawk@mail.com>
2020-03-01 14:40:36 +0600

    added new templates, renamed Templates.json

--- generate_templates.py
+++ generate_templates.py
@@ -18,10 +18,23 @@ templatesMap = {
     "support_app_code_version": 1,
     "templates": [
         {
+            "id": "general",
+            "name": "Общий",
+            "group": "",
+            "laws_links": [
+
+            ],
+            "fields": [
+            ],
+            "text": file_to_string("общий")
+        },
+        {
             "id": "pit_on_road",
             "name": "Яма на дороге",
             "group": "roads",
-            "laws_links": [],
+            "laws_links": [
+
+            ],
             "fields": [
                 {
                     "name": "date_time",
@@ -36,7 +49,7 @@ templatesMap = {
                     "hint": "Опишите дефект"
                 }
             ],
-            "text": file_to_string("pit_on_road")
+            "text": file_to_string("яма_на_дороге")
         },
         {
             "id": "pit_in_yard",
@@ -57,13 +70,18 @@ templatesMap = {
                     "hint": "Опишите дефект"
                 }
             ],
-            "text": file_to_string("pit_on_yard")
+            "text": file_to_string("яма_во_дворе")
         },
         {
             "id": "parking_on_sidewalk",
             "group": "paprking",
             "name": "Парковка на тротуаре",
-            "laws_links": [],
+            "laws_links": [
+                {
+                    "title": "КоАП РФ Статья 12.19. Нарушение правил остановки или стоянки транспортных средств",
+                    "link": "http://www.consultant.ru/document/cons_doc_LAW_34661/b9c0a2b651b7f06f5693cd2e77f04ff473f50f29/"
+                }
+            ],
             "fields": [
                 {
                     "name": "date_time",
@@ -78,13 +96,16 @@ templatesMap = {
                     "hint": "Опишите дефект"
                 }
             ],
-            "text": file_to_string("parking_on_sidewalk")
+            "text": file_to_string("парковка_на_тротуаре")
         },
         {
             "id": "parking_in_wrong place",
             "group": "paprking",
             "name": "Парковка в неположенном месте",
             "laws_links": [
+
+            ],
+            "fields": [
                 {
                     "name": "date_time",
                     "hint": "Время и дата нарушения"
@@ -98,10 +119,9 @@ templatesMap = {
                     "hint": "Опишите дефект"
                 }
             ],
-            "fields": [],
-            "text": file_to_string("parking_in_wrong")
+            "text": file_to_string("парковка_в_неположенном_месте")
         }
     ]
 }
 
-save_to_file("Templates.json", json.dumps(templatesMap))
+save_to_file("templates.json", json.dumps(templatesMap))
