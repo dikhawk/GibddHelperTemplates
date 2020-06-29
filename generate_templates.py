4f6168ffb7f277310235d6f1b75f0d5f8f90e3db
dikhawk <dik_hawk@mail.ru>
2020-06-30 00:09:45 +0600

    updated templates, added new description

--- generate_templates.py
+++ generate_templates.py
@@ -30,10 +30,19 @@ templates_map = {
             "id": "general",
             "name": "Общий",
             "group": "",
+            "description": "",
             "law_links": [
 
             ],
             "fields": [
+                {
+                    "name": "event_location",
+                    "hint": "Укажите место прооисшествия"
+                },
+                {
+                    "name": "appeal_text",
+                    "hint": "Текст обращения"
+                }
             ],
             "text": file_to_string("общий")
         },
@@ -41,20 +50,20 @@ templates_map = {
             "id": "pit_on_road",
             "name": "Яма на дороге",
             "group": "roads",
+            "description": "",
             "law_links": [
-
+                {
+                    "title": "ГОСТ Р 50597-2017. Национальный стандарт Российской Федерации.",
+                    "link": "http://www.consultant.ru/document/cons_doc_LAW_285670/"
+                }
             ],
             "fields": [
                 {
-                    "name": "date_time",
-                    "hint": "Время и дата нарушения"
+                    "name": "event_location",
+                    "hint": "Укажите расположения ямы"
                 },
                 {
-                    "name": "location",
-                    "hint": "Место расположение ямы"
-                },
-                {
-                    "name": "description",
+                    "name": "appeal_text",
                     "hint": "Опишите дефект"
                 }
             ],
@@ -63,19 +72,21 @@ templates_map = {
         {
             "id": "pit_in_yard",
             "group": "roads",
+            "description": "",
             "name": "Яма во дворе",
-            "law_links": [],
+            "law_links": [
+                {
+                    "title": "ГОСТ Р 50597-2017. Национальный стандарт Российской Федерации.",
+                    "link": "http://www.consultant.ru/document/cons_doc_LAW_285670/"
+                }
+            ],
             "fields": [
                 {
-                    "name": "date_time",
-                    "hint": "Время и дата нарушения"
+                    "name": "event_location",
+                    "hint": "Укажите расположения ямы"
                 },
                 {
-                    "name": "location",
-                    "hint": "Место расположение ямы"
-                },
-                {
-                    "name": "description",
+                    "name": "appeal_text",
                     "hint": "Опишите дефект"
                 }
             ],
@@ -84,6 +95,7 @@ templates_map = {
         {
             "id": "parking_on_sidewalk",
             "group": "paprking",
+            "description": "",
             "name": "Парковка на тротуаре",
             "law_links": [
                 {
@@ -93,16 +105,12 @@ templates_map = {
             ],
             "fields": [
                 {
-                    "name": "date_time",
-                    "hint": "Время и дата нарушения"
+                    "name": "event_location",
+                    "hint": "Укажите место нарушения"
                 },
                 {
-                    "name": "location",
-                    "hint": "Место расположения нарушения"
-                },
-                {
-                    "name": "description",
-                    "hint": "Напишите, номер автомобиля, марку и цвет"
+                    "name": "appeal_text",
+                    "hint": "Напишите номер транспортного средства, марку и цвет"
                 }
             ],
             "text": file_to_string("парковка_на_тротуаре")
@@ -110,6 +118,7 @@ templates_map = {
         {
             "id": "disabled_parking",
             "group": "paprking",
+            "description": "По данному обращению вас могу вызвать в ГИБДД в качестве свидетеля.",
             "name": "Парковка на месте для инвалидов",
             "law_links": [
                 {
@@ -119,16 +128,12 @@ templates_map = {
             ],
             "fields": [
                 {
-                    "name": "date_time",
-                    "hint": "Время и дата нарушения"
+                    "name": "event_location",
+                    "hint": "Укажите место нарушения"
                 },
                 {
-                    "name": "location",
-                    "hint": "Место расположения нарушения"
-                },
-                {
-                    "name": "description",
-                    "hint": "Напишите, номер автомобиля, марку и цвет"
+                    "name": "appeal_text",
+                    "hint": "Напишите номер транспортного средства, марку и цвет"
                 }
             ],
             "text": file_to_string("парковка_на_месте_для_инвалидов")
@@ -136,6 +141,7 @@ templates_map = {
         {
             "id": "disabled_parking",
             "group": "paprking",
+            "description": "По данному обращению вас могу вызвать в ГИБДД в качестве свидетеля.",
             "name": "Парковка на месте для инвалидов",
             "law_links": [
                 {
@@ -145,16 +151,12 @@ templates_map = {
             ],
             "fields": [
                 {
-                    "name": "date_time",
-                    "hint": "Время и дата нарушения"
+                    "name": "event_location",
+                    "hint": "Укажите место нарушения"
                 },
                 {
-                    "name": "location",
-                    "hint": "Место расположения нарушения"
-                },
-                {
-                    "name": "description",
-                    "hint": "Напишите, номер автомобиля, марку и цвет"
+                    "name": "appeal_text",
+                    "hint": "Напишите номер транспортного средства, марку и цвет"
                 }
             ],
             "text": file_to_string("парковка_на_месте_для_инвалидов")
@@ -162,58 +164,138 @@ templates_map = {
         {
             "id": "lawn_parking",
             "group": "paprking",
+            "description": "По данному обращению вас могу вызвать в ГИБДД в качестве свидетеля.",
             "name": "Парковка на газоне",
             "law_links": [
+                {
+                    "title": "КоАП РФ Статья 12.19. Нарушение правил остановки или стоянки транспортных средств",
+                    "link": "http://www.consultant.ru/document/cons_doc_LAW_34661/b9c0a2b651b7f06f5693cd2e77f04ff473f50f29/"
+                }
             ],
             "fields": [
+                {
+                    "name": "event_location",
+                    "hint": "Укажите место нарушения"
+                },
+                {
+                    "name": "appeal_text",
+                    "hint": "Напишите номер транспортного средства, марку и цвет"
+                }
             ],
             "text": file_to_string("парковка_на_газоне")
         },
         {
             "id": "pedestrian_parking",
             "group": "paprking",
+            "description": "По данному обращению вас могу вызвать в ГИБДД в качестве свидетеля.",
             "name": "Парковка на пешеходном переходе",
             "law_links": [
+                {
+                    "title": "КоАП РФ Статья 12.19. Нарушение правил остановки или стоянки транспортных средств",
+                    "link": "http://www.consultant.ru/document/cons_doc_LAW_34661/b9c0a2b651b7f06f5693cd2e77f04ff473f50f29/"
+                }
             ],
             "fields": [
+                {
+                    "name": "event_location",
+                    "hint": "Укажите место нарушения"
+                },
+                {
+                    "name": "appeal_text",
+                    "hint": "Напишите номер транспортного средства, марку и цвет"
+                }
             ],
             "text": file_to_string("парковка_на_пешеходном_переходе")
         },
         {
             "id": "prohibited_stop_parking",
             "group": "paprking",
+            "description": "По данному обращению вас могу вызвать в ГИБДД в качестве свидетеля.",
             "name": "Парковка под знаком Остановка запрещена",
             "law_links": [
+                {
+                    "title": "КоАП РФ Статья 12.19. Нарушение правил остановки или стоянки транспортных средств",
+                    "link": "http://www.consultant.ru/document/cons_doc_LAW_34661/b9c0a2b651b7f06f5693cd2e77f04ff473f50f29/"
+                }
             ],
             "fields": [
+                {
+                    "name": "event_location",
+                    "hint": "Укажите место нарушения"
+                },
+                {
+                    "name": "appeal_text",
+                    "hint": "Напишите номер транспортного средства, марку и цвет"
+                }
             ],
             "text": file_to_string("парковка_под_знаком_остановка_запрещена")
         },
         {
             "id": "traffic_light_problems",
             "group": "road_infrastructure",
+            "description": "",
             "name": "Проблемы со светофором",
             "law_links": [
+                {
+                    "title": "КоАП РФ Статья 12.34. Несоблюдение требований по обеспечению безопасности дорожного движения при строительстве, реконструкции, ремонте и содержании дорог, железнодорожных переездов или других дорожных сооружений",
+                    "link": "http://www.consultant.ru/document/cons_doc_LAW_34661/e6550c7aaf87a7eba570e0dfb5c9d1f86b84298f/"
+                }
             ],
             "fields": [
+                {
+                    "name": "event_location",
+                    "hint": "Укажите место"
+                },
+                {
+                    "name": "appeal_text",
+                    "hint": "Опишите какой дефект вы обнаружили"
+                }
             ],
             "text": file_to_string("проблемы_со_светофором")
         },
         {
             "id": "oncoming_traffic_on_oneway_road",
             "group": "roads",
+            "description": "По данному обращению вас могу вызвать в ГИБДД в качестве свидетеля.",
             "name": "Движение во встречном направлении по дороге с односторонним движением",
-            "law_links": [],
+            "law_links": [
+                {
+                    "title": "КоАП РФ Статья 12.16. Несоблюдение требований, предписанных дорожными знаками или разметкой проезжей части дороги",
+                    "link": "http://www.consultant.ru/document/cons_doc_LAW_34661/423d650543917f5abe5c2480d6fb3fca332f9d22/"
+                }
+            ],
             "fields": [
+                {
+                    "name": "event_location",
+                    "hint": "Укажите место нарушения"
+                },
+                {
+                    "name": "appeal_text",
+                    "hint": "Напишите номер транспортного средства, марку и цвет"
+                }
             ],
             "text": file_to_string("движение_во_встречном_направлении_по_дороге_с_односторонним_движением")
         },
         {
             "id": "do_not_provide_an_advantage_at_the_crosswalk",
             "group": "roads",
+            "description": "По данному обращению вас могу вызвать в ГИБДД в качестве свидетеля.",
             "name": "Не пропустили на пешеходном переходе",
-            "law_links": [],
+            "law_links": [
+                {
+                    "title": "КоАП РФ Статья 12.18. Непредоставление преимущества в движении пешеходам или иным участникам дорожного движения",
+                    "link": "http://www.consultant.ru/document/cons_doc_LAW_34661/b2daf1d5306cf52a5bb0ec8a1bc0ced772a4f162/"
+                }
+            ],
             "fields": [
+                {
+                    "name": "event_location",
+                    "hint": "Укажите место нарушения"
+                },
+                {
+                    "name": "appeal_text",
+                    "hint": "Напишите номер транспортного средства, марку и цвет"
+                }
             ],
             "text": file_to_string("не_пропустили_на_пешеходном_переходе")
         }
